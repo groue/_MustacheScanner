73efb525c91402ac5b38db2631ab3eba5daf7cb0
Gwendal Roué <gr@pierlis.com>
2015-12-09 13:11:50 +0100

    Initial Commit

---
+++ StringParsing/StringParsing.h
@@ -0,0 +1,19 @@
+//
+//  StringParsing.h
+//  StringParsing
+//
+//  Created by Gwendal Roué on 09/12/2015.
+//  Copyright © 2015 Pierlis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StringParsing.
+FOUNDATION_EXPORT double StringParsingVersionNumber;
+
+//! Project version string for StringParsing.
+FOUNDATION_EXPORT const unsigned char StringParsingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringParsing/PublicHeader.h>
+
+
